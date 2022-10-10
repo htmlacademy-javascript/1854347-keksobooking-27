231a253a88ae8d3463c5a2586ca0f65a0f714908
AnDenki <todenki003@gmail.com>
2022-10-10 23:27:45 +0300

    fix error 2

--- js/main.js
+++ js/main.js
@@ -3,19 +3,19 @@ function getRandomInteger (min, max) {
     const a = Math.min(Math.abs(min), Math.abs(max));
     const b = Math.max(Math.abs(min), Math.abs(max));
     return Math.trunc(Math.random() * (b - a)) + a;
-      } return 'НЕ число'
+  } else {return 'НЕ число'}
 }
 
-function getCordinatePoint (min, max, comma=1) {
+function getCordinatePoint (min, max, comma = 1) {
   if (isFinite(min) && isFinite(max) && isFinite(comma)) {
     const a = Math.min(Math.abs(min), Math.abs(max));
     const b = Math.max(Math.abs(min), Math.abs(max));
-    const num =Math.random() * (b - a) + a;
+    const num = Math.random() * (b - a) + a;
     return num.toFixed(Math.abs(comma));
-      } else {
-        return 'Не число!!';
-      }
+  } else {
+    return 'Не число!!';
+  }
 }
 
 getRandomInteger(1, 100);
-getCordinatePoint(1, 2000, 2.2);
+getCordinatePoint(1, 2000, 2);
