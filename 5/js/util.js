9845d526a9ef49a924936d0de167fd5c6a6e484e
Keks <keks@htmlacademy.ru>
2022-11-06 00:14:32 +0300

    :heavy_check_mark: Сборка #5

---
+++ 5/js/util.js
@@ -0,0 +1,34 @@
+export const getRandomInteger = (min, max) => {
+  if (isFinite(min) && isFinite(max)) {
+    const a = Math.min(Math.abs(min), Math.abs(max));
+    const b = Math.max(Math.abs(min), Math.abs(max));
+    return Math.trunc(Math.random() * (b - a)) + a;
+  } else {
+    return 'Не число!!';
+  }
+};
+
+export const getCordinatePoint = (min, max, comma = 1) => {
+  if (isFinite(min) && isFinite(max) && isFinite(comma)) {
+    const a = Math.min(Math.abs(min), Math.abs(max));
+    const b = Math.max(Math.abs(min), Math.abs(max));
+    const num = Math.random() * (b - a) + a;
+    return num.toFixed(Math.abs(comma));
+  } else {
+    return 'Не число!!';
+  }
+};
+
+export const randomNumber = () => {
+  const id = getRandomInteger(1, 10);
+  if (id < 10) {
+    return (`0${id}`);
+  } else {
+    return ( `${id}` );
+  }
+};
+
+export const arrayRandElement = (items) => items[getRandomInteger(0, items.length - 1)];
+
+export default {getRandomInteger, getCordinatePoint, arrayRandElement, randomNumber};
+
