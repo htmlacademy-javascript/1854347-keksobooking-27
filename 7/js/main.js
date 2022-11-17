4c9535d98a7d9d89fbee075fbca02c393dfaef76
Keks <keks@htmlacademy.ru>
2022-11-17 15:46:54 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/main.js
@@ -0,0 +1,9 @@
+import { creatArrElements } from './card.js';
+import { offerToCard } from './map.js';
+import './form.js';
+
+const offers = creatArrElements();
+const cards = offers.map(offerToCard);
+
+const blockMap = document.querySelector('#map-canvas');
+blockMap.append(cards[0]);
