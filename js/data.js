f359f1190e50c59874efdaa435044ab9a3a9ecf2
AnDenki <todenki003@gmail.com>
2022-11-20 17:05:02 +0300

    12.14. Перламутровые пуговицы — для сложных проектов

--- js/data.js
+++ js/data.js
@@ -4,7 +4,6 @@ import { getRandomInteger, getRandArray, arrayRandElement } from './util.js';
 const getAvatarFunction = () => () => `img/avatars/user${getRandomInteger(1, 10)}.png`;
 
 export const getAvatar = getAvatarFunction();
-
 export const getRandomFeatures = () => getRandArray(FEATURES);
 export const getRandomPhotos = () => getRandArray(PHOTOS);
 export const getRandomDescription = () => arrayRandElement(DESCRIPTIONS);
